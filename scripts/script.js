38db7a490d0566da490e291c137696cf650f21a6
Rafael Lacuesta <81370636+RafOs14@users.noreply.github.com>
2022-06-02 20:32:17 -0300

    Add files via upload

---
+++ scripts/script.js
@@ -0,0 +1,45 @@
+//Anclajes a las secciones de la pagina
+$(function(){
+    $('a[href*=#ancla]').click(function() {
+        if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+            var $target = $(this.hash);
+            $target = $target.length && $target || $('[name=' + this.hash.slice(1) +']');
+            if ($target.length) {
+                var targetOffset = $target.offset().top;
+                $('html,body').animate({scrollTop: targetOffset}, 800);
+                return true;
+            }
+        }
+    });
+});
+
+
+
+/*Back to top button*/
+// //Get the button
+// let mybutton = document.getElementById("btn-back-to-top");
+
+// // When the user scrolls down 20px from the top of the document, show the button
+// window.onscroll = function () {
+//   scrollFunction();
+// };
+
+// function scrollFunction() {
+//   if (
+//     document.body.scrollTop > 20 ||
+//     document.documentElement.scrollTop > 20
+//   ) {
+//     mybutton.style.display = "block";
+//   } else {
+//     mybutton.style.display = "none";
+//   }
+// }
+// // When the user clicks on the button, scroll to the top of the document
+// mybutton.addEventListener("click", backToTop);
+
+// function backToTop() {
+//   document.body.scrollTop = 0;
+//   document.documentElement.scrollTop = 0;
+// }
+
+/* Fin de back to top button*/
